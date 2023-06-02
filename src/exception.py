ad0939a2ea5f1be503679b32895184e385f3a1c0
Kaushikascoder-26 <kaushik.sheet.26@gmail.com>
2023-06-02 22:58:33 +0530

    completed with data_ingestion and data_transformation

--- src/exception.py
+++ src/exception.py
@@ -2,11 +2,9 @@ import sys
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exec_tb.tb_frame.f_code.co_filenmae
-    error_message = "Error occured in pyhton script name [{0}] line number [{1}] error message[{2}]".format()
-    file_name,exc_tb.tb_lineno,str(error)
-    
+    _,_,exec_tb=error_detail.exc_info()
+    file_name=exec_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in pyhton script name [{0}] line number [{1}] error message[{2}]".format(file_name, exec_tb.tb_lineno, str(error))
     return error_message
     
 class CustomException(Exception):
@@ -17,10 +15,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__=="__main__":
-    
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomerException(e,sys)
